dce85e0acbfa56fa69a509adb6013660819c7233
chile <anphawolf@gmail.com>
2023-04-02 17:45:47 +0700

    added twitter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import LanguageSelector from './LocaleSelector';
 import AppearanceSelector from './AppearanceSelector';
 import React from 'react';
 import SpeechGPTIcon from './Icons/SpeechGPTIcon';
+import TwitterIcon from './Icons/TwitterIcon';
 
 
 
@@ -9,7 +10,10 @@ import SpeechGPTIcon from './Icons/SpeechGPTIcon';
 function Header() {
   return (
     <div className="flex flex-col sm:pt-20 sticky pt-16">
+      <div>
       <SpeechGPTIcon className="w-16 h-16 ml-2 sm:w-24 sm:h-24" />
+      </div>
+      
       <div className="flex flex-row py-2 justify-between items-center w-full">
         <div className="text-2xl font-bold text-left bg-gray-100 text-gray-800">
           <span className="font-bold ml-2 decoration-purple-500 animate-text text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
@@ -27,10 +31,14 @@ function Header() {
             window.open('https://app.mute.io', '_blank');
             }}
         >
-          
+                
           BUY $ZKGPT
         </button>
+        
         </div>
+        <a href='https://twitter.com/ZKSpeechGPT' target={"_blank"}>
+          <TwitterIcon/>
+        </a>
         <div>
           <LanguageSelector />
           {/*<AppearanceSelector/>*/}
